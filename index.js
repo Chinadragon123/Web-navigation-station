342d671eceb8f90a243e8b31e3f1958081646708
Ch_dragon233 <128207615+Chinadragon123@users.noreply.github.com>
2024-01-26 22:06:38 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+document.getElementById("wechat").addEventListener('click', function() {
+    window.location.href = 'https://weixin.qq.com';
+});
+
+document.getElementById("QQ").addEventListener('click', function() {
+    window.location.href = 'https://im.qq.com';
+});
+
+document.getElementById("dingtalk").addEventListener('click', function() {
+    window.location.href = 'https://www.dingtalk.com';
+});
+
+document.getElementById("360").addEventListener('click', function() {
+    window.location.href = 'https://www.360.com';
+});
+
+document.getElementById("huorong").addEventListener('click', function() {
+    window.location.href = 'https://www.huorong.cn';
+});
+
+document.getElementById("chrome").addEventListener('click', function() {
+    window.location.href = 'https://www.google.cn/chrome';
+});
+
+document.getElementById("wps").addEventListener('click', function() {
+    window.location.href = 'https://www.wps.cn/';
+});
